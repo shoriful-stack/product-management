866ae17df176c69e87a679cefe075aaaf30be64a
Md. Shoriful Islam <sisazid182713@gmail.com>
2025-11-17 21:20:26 +0600

    feat: template mastering

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>@yield('title')</title>
+    @include('partials.header')
+</head>
+
+<body>
+    <main>
+        @yield('content')
+        @include('partials.footer')
+    </main>
+</body>
+
+</html>
